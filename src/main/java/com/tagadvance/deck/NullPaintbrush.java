8045a235641e636422e58be6b04a1324394d9ab3
Tag <tagspilman@gmail.com>
2017-08-26 22:14:06 -0700

    NullPaintbrush

---
+++ src/main/java/com/tagadvance/deck/NullPaintbrush.java
@@ -0,0 +1,16 @@
+package com.tagadvance.deck;
+
+import java.awt.Graphics2D;
+
+public class NullPaintbrush implements Paintbrush {
+
+	public NullPaintbrush() {
+		super();
+	}
+
+	@Override
+	public void paint(Graphics2D g) {
+
+	}
+
+}
